720ade444ef65173d756ce6d152db04fb33f3db4
Victor Tran <vicr12345@gmail.com>
2025-10-29 18:22:50 +1100

    Render context menu in list as raised

--- thegrid/src/chat/sidebar/standard_room_element.rs
+++ thegrid/src/chat/sidebar/standard_room_element.rs
@@ -199,7 +199,7 @@ impl RenderOnce for StandardRoomElement {
             .on_click(move |event, window, cx| {
                 on_click(event, window, cx);
             })
-            .with_deferred_context_menu(context_menu)
+            .with_context_menu(context_menu)
             .child(
                 dialog_box("leave-room-dialog-box")
                     .render_as_deferred(true)
